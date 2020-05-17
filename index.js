28c5793a8550f08e18587bb8fc373920243625eb
Daniel Byrd <admin@centsibly.io>
2020-05-16 21:37:02 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const core = require('@actions/core');
+const aws = require('aws-sdk');
+
+const WAIT_DEFAULT_DELAY_SEC = 15;
+
+// Deploy to a service that uses the 'ECS' deployment controller
+async function runTask(ecs, clusterName, taskName, waitForMinutes) {
+  core.debug('Running the task');
+  const taskInfo = await ecs.runTask({
+    cluster: clusterName,
+    taskDefinition: taskName,
+  }).promise();
+  core.info(`Task started.`);
+
+  // Wait for service stability
+  core.debug(`Waiting for the service to become stable. Will wait for ${waitForMinutes} minutes`);
+  const maxAttempts = (waitForMinutes * 60) / WAIT_DEFAULT_DELAY_SEC;
+  await ecs.waitFor('tasksStopped', {
+    tasks: [taskInfo.tasks.map(t => t.taskArn)],
+    cluster: clusterName,
+    $waiter: {
+      delay: WAIT_DEFAULT_DELAY_SEC,
+      maxAttempts: maxAttempts
+    }
+  }).promise();
+}
+
+async function run() {
+  try {
+    const ecs = new aws.ECS({
+      customUserAgent: 'centsibly-run-task-for-github-actions'
+    });
+
+    // Get inputs
+    const clusterName = core.getInput('cluster', { required: true});
+    const task = core.getInput('task', { required: true});
+    let waitForMinutes = 30 
+
+    await runTask(ecs, clusterName, task, waitForMinutes);
+  }
+  catch (error) {
+    core.setFailed(error.message);
+    core.debug(error.stack);
+  }
+}
+
+module.exports = run;
+
+/* istanbul ignore next */
+if (require.main === module) {
+    run();
+}
